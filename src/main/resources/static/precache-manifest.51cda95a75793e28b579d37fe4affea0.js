26f8dfe9d347f732bee7f7d1618d177e262e6378
Siddhant Giri <sidgiri2000@gmail.com>
2020-09-11 10:15:51 +0530

    frontend added

---
+++ src/main/resources/static/precache-manifest.51cda95a75793e28b579d37fe4affea0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30df41047781dd1c9966bb00163583cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea8545a66596157a188",
+    "url": "/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "d0db1e4edcc7ecfdf1ec",
+    "url": "/static/css/main.cb168a0c.chunk.css"
+  },
+  {
+    "revision": "3ea8545a66596157a188",
+    "url": "/static/js/2.94d92e5b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.94d92e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0db1e4edcc7ecfdf1ec",
+    "url": "/static/js/main.05578038.chunk.js"
+  },
+  {
+    "revision": "b5b69312c6b5d3e86629",
+    "url": "/static/js/runtime-main.746ba068.js"
+  },
+  {
+    "revision": "b617b31498c257d4e3bca9834975ae76",
+    "url": "/static/media/aboutcoursify.b617b314.png"
+  },
+  {
+    "revision": "e78dd91f2b0188e575881ba3a8b7bd4d",
+    "url": "/static/media/coursifybgimg.e78dd91f.jpg"
+  },
+  {
+    "revision": "3525c475875105cd99660d815dad0acf",
+    "url": "/static/media/coursifyhome.3525c475.png"
+  }
+]);
